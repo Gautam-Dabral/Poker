d16d619a8b551677b130b05927ae0b7393e9e0de
pankaj rawat <pankajrawat7577@gmail.com>
2021-05-29 08:17:59 +0530

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -3,17 +3,26 @@
 #include<time.h>
 using namespace std;
 
-typedef enum value {ace=0,two=1,three=2,four=3,five=4,six=5,seven=6,eight=7,nine=8,ten=9,jack=10,queen=11,king=12}value;
-typedef enum suit {spades=0,hearts=1,diamonds=2,clubs=3}suit;
+//typedef enum value {ace=0,two=1,three=2,four=3,five=4,six=5,seven=6,eight=7,nine=8,ten=9,jack=10,queen=11,king=12}value;
+//typedef enum suit {spades=0,hearts=1,diamonds=2,clubs=3}suit;
+
+
+string suits[]={"spades","hearts","diamonds","clubs"};
+string value[]={"ace","two","three","four","five","six","seven",
+"eight","nine","ten","jack","queen","king"};
+
+
 class card
 {
 private :
-    int v;
-    int s;
+    string v;
+    string s;
 public :
     void setcard (int x)                   // random integers x and y being passed as arguments
-    {v=(x%13);
-    s=(x%4);}
+    {
+	v=value[(x%13)];
+    	s=suit[(x%4)];
+     }
     card (int x)
     {cout<<"too many arguments";}
     card ()
@@ -23,7 +32,7 @@ public :
 };
 ostream& operator<<(ostream& dout, card C)           //overloading operator << for card type data
     {
-        dout<<C.v<<" "<<C.s<<endl;
+        dout<<C.v<<" of "<<C.s<<endl;
         return(dout);
     }
 
