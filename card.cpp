2750af5a65d2f8beb45169a195608793617b2906
Gautam Dabral <gautamdabral1999@gmail.com>
2021-05-29 09:01:50 +0530

    randomly generates 5 cards in numerical values of card and suit

--- card.cpp
+++ card.cpp
@@ -3,16 +3,14 @@
 #include<time.h>
 using namespace std;
 
-typedef enum value {ace=0,two=1,three=2,four=3,five=4,six=5,seven=6,eight=7,nine=8,ten=9,jack=10,queen=11,king=12}value;
-typedef enum suit {spades=0,hearts=1,diamonds=2,clubs=3}suit;
 class card
 {
 private :
-    value v;
-    suit s;
+    int v;
+    int s;
 public :
-    void setcard (int x, int y)                   // random integers x and y being passed as arguments
-    {v=(x%13); s=(y%4);}
+    void setcard (int x)                   // random integers x and y being passed as arguments
+    {v=(x%13); s=(x%4);}
     card (int x)
     {cout<<"too many arguments";}
     card ()
@@ -22,7 +20,7 @@ public :
 };
 ostream& operator<<(ostream& dout, card C)           //overloading operator << for card type data
     {
-        dout<<C.v<<" of "<<C.s;
+        dout<<C.v<<" of "<<C.s<<endl;
         return(dout);
     }
 
@@ -34,9 +32,7 @@ int main ()
     srand(seed);
     for (i=0; i<5; i++)
     {
-        x=rand();
-        y=rand();
-        c1.setcard(x,y);
+        c1.setcard(rand());
         cout<<c1;
     }
 
