3679d7555ef679bb4da3abd2edbfcec128c5f13b
Gautam Dabral <gautamdabral1999@gmail.com>
2021-05-29 09:03:59 +0530

    copied card.cpp into main.cpp

--- main.cpp
+++ main.cpp
@@ -3,26 +3,14 @@
 #include<time.h>
 using namespace std;
 
-//typedef enum value {ace=0,two=1,three=2,four=3,five=4,six=5,seven=6,eight=7,nine=8,ten=9,jack=10,queen=11,king=12}value;
-//typedef enum suit {spades=0,hearts=1,diamonds=2,clubs=3}suit;
-
-
-string suits[]={"spades","hearts","diamonds","clubs"};
-string value[]={"ace","two","three","four","five","six","seven",
-"eight","nine","ten","jack","queen","king"};
-
-
 class card
 {
 private :
-    string v;
-    string s;
+    int v;
+    int s;
 public :
     void setcard (int x)                   // random integers x and y being passed as arguments
-    {
-	v=value[(x%13)];
-    	s=suits[(x%4)];
-     }
+    {v=(x%13); s=(x%4);}
     card (int x)
     {cout<<"too many arguments";}
     card ()
@@ -38,7 +26,7 @@ ostream& operator<<(ostream& dout, card C)           //overloading operator << f
 
 int main ()
 {
-    int seed,i;
+    int seed,i,x,y;
     card c1;
     seed=time(NULL);
     srand(seed);
