555cfe04ea16d254ba5f873323d5a822cb24f297
Gautam Dabral <gautamdabral1999@gmail.com>
2021-06-05 13:29:48 +0530

    Version-3.1 evaluates for pair, 3s , rf, sf, s, f

--- logic.cpp
+++ logic.cpp
@@ -2,7 +2,8 @@
 #include<cstdbool>
 
 using namespace std;
-card c_high;
+card c_high; c_kicker;
+int k=0;
 
 void sort_by_value ()
 {
@@ -70,29 +71,30 @@ bool isflush (int i)                  // i being the player
 
 bool isstraight (int i)
 {
-    int k=0;
     sort_by_value ();
     if(p[i].c[0].value==0)
     {
-        if(p[i].c[4].value==4)
-        {c_high.value=4;
-        c_high.suit=p[i].c[4].suit;
-        return(true);}
-        else if (p[i].c[3].value==9)
-        {c_high.value=13;
-        c_high.suit=p[i].c[0].suit;
-        return (true);}
+        if(p[i].c[1].value==1&&p[i].c[2].value==2&&p[i].c[3].value==3&&p[i].c[4].value==4)
+           {c_high.value=4;
+            return (true);}
+        else if(p[i].c[3].value==9&&p[i].c[4].value==10&&p[i].c[5].value==11&&p[i].c[6].value==12)
+            {c_high.value=13;
+            return (true);}
         else
-        {}
-    }
-    else if (p[i].c[0].value+4==p[i].c[4].value)
-    {
-        c_high.value=p[i].c[4].value;
-        c_high.suit=p[i].c[4].suit;
-        return (true);
+            {return (false);}
     }
     else
-        return (false);
+    {
+        for(j=0; j<3; j++)
+        {int temp=p[i].c[j].value;
+        if(p[i].c[j+1].value==temp+1&&p[i].c[j+2].value==temp+2&&p[i].c[j+3].value==temp+3&&p[i].c[j+4].value==temp+4)
+        {c_high.value=p[i].c[j+4].value;
+        return (true);}
+        else
+        {return (false);}
+        }
+
+    }
 
 }
 
@@ -103,27 +105,60 @@ bool isstraight_flush (int i)
     else
     {return(false);}
 }
+bool isroyal_flush (int i)
+{
+    if(isstraight_flush(int i)&&c_high.value==0)
+        return (true);
+    else
+        return (false);
+}
+bool isfour_of_a_kind (int i)
+{
+    sort_by_value();
+    for(j=0; j<4; j++)
+    {
+        if(p[i].c[j].value==p[i].c[j+3].value)
+        {c_high.value=p[i].c[j].value;
+        (c_high.value==0) ? c_high.value=13 : ;
+        (j==3) ? (p[i].c[0]==0) ? c_kicker.value=13 : c_kicker.value=p[i].c[2].value : c_kicker.value=p[i].c[6].value ;
+        return (true);}
+    }
+    if(j>3)
+        {return (false);}
+}
+
+bool isthree_of_a_kind (int i)
+{
+    sort_by_value();
+    for(j=0; j<5; j++)
+    {
+        if(p[i].c[j].value==p[i].c[j+2].value)
+        {c_high.value=p[i].c[j].value;
+        (c_high.value==0) ? c_high.value=13 : ;
+        (j==4) ? (p[i].c[0]==0) ? c_kicker.value=13 : c_kicker.value=p[i].c[2].value : c_kicker.value=p[i].c[6].value ;
+        return (true);}
+    }
+    if(j>4)
+    {return (false);}
+}
 bool ispair (int i)
 {
-    int k=0;
     sort_by_value();
     for(j=0; j<6; j++)
     {
         if(p[i].c[j].value==p[i].c[j+1].value)
-        {c_high.value=p[i].c[j].value;
-        k++;}
-
-        else
-        {}
-    }
-    if(k>0)
         {
-            if(p[i].c[0].value==0)        // when a player's hand has an ace
-                {c_high.value=13;}
-            else
-                {};
-            return(true);
+            c_high.value=p[i].c[j].value;
+            (c_high.value==0) ? c_high.value=13 : ;
+        if(j<5)
+        (p[i].c[0].value==0) ? c_kicker.value=13 : c_kicker.value=p[i].c[6].value ;
+        else
+        (p[i].c[0].value==0) ? c_kicker.value=13 : c_kicker.value=p[i].c[4].value ;
+
+        return (true);
         }
-    else
-        return (false);
+    }
+    if(j>=5)
+        return(false);
 }
+
